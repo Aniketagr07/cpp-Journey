4d2b6550ffec83a3053b90583ef0fc9e0b315031
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-07 23:29:35 +0530

    Learning about Variable

---
+++ Variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=25;
+    char ch='S';
+    float f=3.14f;
+    bool b=false;
+    double d=3.141592653589793;
+    cout<<age<<' '<<ch<<' '<<f<<' '<<b<<' '<<d<<endl;
+    return 0;
+}
