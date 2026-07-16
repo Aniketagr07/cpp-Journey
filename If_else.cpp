1d68daea9375f9e56b1e685f73f66232e806b127
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-16 13:01:07 +0530

    Learning the Syntax of If Else statement

---
+++ If_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a No.:- ";
+    cin>>n;
+    if(n>0){
+        cout<<n<<" is a positive no."<<endl;
+    }
+    else{
+        cout<<n<<" is a negative no."<<endl;
+    }
+    return 0;
+}
