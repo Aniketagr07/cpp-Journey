3063afae8578c45a1a3d6a6f7b6c61f4176d5e54
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-16 12:51:29 +0530

    Creating my First Working code by taking input doing some operation and giving output

---
+++ Sum_of_two_no.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First No.:- ";
+    cin>>a;
+    cout<<"Enter Second No.:- ";
+    cin>>b;
+    cout<<"Sum of the two no is:- "<<(a+b)<<endl;
+    return 0;
+}
