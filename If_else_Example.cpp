1ffd2c5451b0e4c21b598b687a60d1de713b42a8
Aniketagr07 <aniket.agr2007@gmail.com>
2026-07-19 11:48:20 +0530

    If Else Example Questions

---
+++ If_else_Example.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int age;
+    cout<<"Enter your age:- ";
+    cin>>age;
+    if(age>=18){
+        cout<<"You are eligible to give vote"<<endl;
+    }else{
+        cout<<"You are not eligible to give vote"<<endl;
+    }
+    */
+   int n;
+   cout<<"Enter a No.:- ";
+   cin>>n;
+   if(n%2==0){
+    cout<<n<<" is a even no."<<endl;
+   }else{
+    cout<<n<<" is a odd no."<<endl;
+   }
+    return 0;
+}
